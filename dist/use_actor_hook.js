992eaacd35e181c980ce84556b5f68f5b17f2c2a
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2023-03-10 17:51:47 +0100

    fix(build): add the type declarations

---
+++ dist/use_actor_hook.js
@@ -0,0 +1,14 @@
+import { useSyncExternalStore } from 'use-sync-external-store';
+export const useActorState = (actor) => {
+    function subscribe(callback) {
+        const subId = +new Date();
+        actor.ref.__reactSubs.set(subId, callback);
+        return () => {
+            actor.ref.__reactSubs.delete(subId);
+        };
+    }
+    function getSnapshot() {
+        return actor.ref.__reactState;
+    }
+    return useSyncExternalStore(subscribe, getSnapshot);
+};
