9a5afa7ceedeb876fb0318b94c5017b59e6ea631
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2023-11-06 23:07:55 +0100

    refactor(build): clean files

--- dist/react_materializer.d.ts
+++
@@ -1,7 +0,0 @@
-import { Actor, ActorMessage, IMaterializer } from 'tarant';
-export declare class ReactMaterializer implements IMaterializer {
-    onInitialize(actor: Actor | any): void;
-    onBeforeMessage(actor: Actor | any, message: ActorMessage): void;
-    onAfterMessage(actor: Actor | any, message: ActorMessage): void;
-    onError(actor: Actor | any, message: ActorMessage, error: any): void;
-}
